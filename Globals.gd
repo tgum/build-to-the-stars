1251a050c6e3e1c67ef4fd559694a79122968652
MatricsCode <jayden4er@posteo.de>
2024-09-13 09:55:03 +0200

    Fixed Restarting

--- Globals.gd
+++ Globals.gd
@@ -42,7 +42,35 @@ var win_game = false
 
 var drop_height
 
-func _ready():
+func reload():
+	ghost_block_textures = []
+	placed_block_textures = []
+	houselet_shapes = [
+	[Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(1, 1)], # T
+	[Vector2(0, 0), Vector2(1, 0), Vector2(1, 1), Vector2(2, 1)], # Z
+	[Vector2(0, 0), Vector2(0, 1), Vector2(1, 1), Vector2(2, 1), Vector2(2, 0)], # U
+	[Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(3, 0)], # I
+	[Vector2(0, 0), Vector2(1, 0), Vector2(1, -1), Vector2(2, -1)], # S
+	[Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(2, 1)], # L
+	[Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(2, -1)], # upside down L
+	]
+	houselet_pivots = []
+	can_drop = true
+	next_block = {
+	"rotation": 0,
+	"x": 0,
+	"y": 0,
+	"shape_i": 0,
+	"texture_i": 0
+}
+	tile_size = 32
+	screen_dimensions
+	music_play = false
+	game_over = false
+	win_game = false
+	drop_height
+	get_tree().paused = false
+	
 	for i in range(1, 6):
 		ghost_block_textures.append(load("res://assets/Buildings/Dotted Buildings"+str(i)+".png"))
 		placed_block_textures.append(load("res://assets/Buildings/Buildings"+str(i)+".png"))
@@ -60,8 +88,20 @@ func _ready():
 	
 	drop_height = tile_size*3
 
-func reload():
-	game_over = false
-	win_game = false
-	get_tree().paused = false
-	get_tree().reload_current_scene()
+func _ready():
+	for i in range(1, 6):
+		ghost_block_textures.append(load("res://assets/Buildings/Dotted Buildings"+str(i)+".png"))
+		placed_block_textures.append(load("res://assets/Buildings/Buildings"+str(i)+".png"))
+	
+	for houselet in houselet_shapes:
+		var avg = Vector2.ZERO
+		for pos in houselet:
+			avg += pos
+		avg /= houselet.size()
+		houselet_pivots.append(avg)
+	
+	var camera = get_viewport().get_camera_2d()
+	var screen_size = get_viewport().get_size()
+	screen_dimensions = Vector2(screen_size) / camera.zoom
+	
+	drop_height = tile_size*3
--- Start_scene.gd
+++ Start_scene.gd
@@ -3,5 +3,5 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().create_timer(randf_range(1,10)).timeout
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://main.tscn")
